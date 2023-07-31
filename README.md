b362292b662978bd0ace7058933860aaa258bf80
KBKUN024 <944412796@qq.com>
2023-07-31 17:45:40 +0800

    更新了Readme.md,更新了详细的中文注释

--- README.md
+++ README.md
@@ -1,6 +1,56 @@
-# TextClamp Component for Vue3.0
+<h1 align="center">TextClamp Component for Vue3.0</h1>
+<p align="center">
+  <a href="https://www.npmjs.com/package/text-clamp-for-vue3"><img src="https://badgen.net/npm/v/text-clamp-for-vue3" alt="npm package"></a>
+</p>
+
+> *This component is based on the text expansion and collapse component developed by vue 3.0, maybe the simplest and most understandable version of the code on github now, I hope it can help people who need it, if you think the writing is good, welcome to give me a star.*
+<br/>
+
+- Customizable button types
+- Customizable text lines
+- Easy to understand
+
+## **Install**
+
+```
+    npm install text-clamp-for-vue3
+```
+
+## **Full import**
+
+```javascript
+    import { createApp } from "vue";
+    import TextClamp from 'text-clamp-for-vue3'
+    import App from "./App.vue";
+    const app = createApp(App);
+    app.use(TextClamp).mount("#app");
+```
+## **Usage**
+
+```javascript
+    <TextClamp :text="str" :buttonType="'tight'" :maxLines="4">
+        <template #textExpandButton="props">
+          <div v-if="props.buttonType == 'oneLine'" :style="{
+            textAlign: 'left',
+            cursor: 'pointer',
+            display: 'flex',
+            justifyContent: 'flex-end'
+          }">
+            <button @click="props.toggle">
+              {{ props.isExpanded ? "Collapse" : "Expand" }}
+            </button>
+          </div>
+          <button @click="props.toggle" v-else>
+            {{ props.isExpanded ? "Collapse" : "Expand" }}
+          </button>
+        </template>
+      </TextClamp>
+```
+
 ## **Properties**
-### **text: string;** // *Component content,it's Required.*
-### **buttonType?: 'oneLine' | 'tight';** // *The unfold collapse button is divided into: 1. oneLine: Occupies a single line 2. tight: Closely adjacent to text*
-### **maxLines?: number;** // *Sets the number of rows displayed*
-### **isExpanded?: boolean;** // *Expanded state, true: expanded, false: collapsed*
+
+| text: string                      | Component content,it's Required.                                                                                 |
+| --------------------------------- | ---------------------------------------------------------------------------------------------------------------- |
+| buttonType?: 'oneLine' \| 'tight' | The unfold collapse button is divided into: 1. oneLine:Occupies a single line 2. tight: Closely adjacent to text |
+| maxLines?: number                 | Sets the number of rows displayed                                                                                |
+| isExpanded?: boolean              | Expanded state, true: expanded, false: collapsed                                                                 |
--- src/package/TextClamp.vue
+++ src/package/TextClamp.vue
@@ -13,7 +13,6 @@ const props = withDefaults(defineProps<{
     maxLines: 3
 })
 const textClampRef = ref<HTMLElement | null>(null) // 最外层div的ref
-// const textClampContainerRef = ref<HTMLElement | null>(null) // 该组件放内容的容器的ref
 const textRef = ref<HTMLElement | null>(null) // 该组件放文本内容的ref
 const toggleButtonRef = ref<HTMLElement | null>(null)
 const expanded = ref(props.isExpanded) //本地的expanded状态，先获取一遍属性中的isExpanded状态，然后才能用toggle方法进行修改
@@ -26,17 +25,22 @@ const clampClass = ref({
     'overflow': 'hidden',
     'text-overflow': 'ellipsis'
 })
+/**
+ * 将文本截取标志offset往右移动一个单位，同时截取文本并赋值
+ */
 function moveOffsetRight() {
     offset.value = offset.value + 1
     textRef.value && (textRef.value.textContent = props.text.slice(0, offset.value) + '...')
 }
-// 往左移动一个步长
+/**
+ * 将文本截取标志offset往左移动一个单位，同时截取文本并赋值
+ */
 function moveOffsetLeft() {
     offset.value = offset.value - 1
     textRef.value && (textRef.value.textContent = props.text.slice(0, offset.value) + '...')
 }
 /**
- * 获取一下展开收起按钮的宽度
+ * 获取一下展开收起按钮的宽度以及文本容器的宽度
  */
 function getButtonWidth() {
     // 按钮
@@ -61,8 +65,12 @@ function getRects() {
     }
     return { rects, rectsLength }
 }
+/**
+ * 收紧文本
+ * ?实现思路：比较按钮宽度加最后一个矩形框的宽度是否会大于文本容器宽度，若大于，递归调用自身，即调用moveOffsetLeft()，进一步缩紧文本，否则直接return
+ */
 function tightText() {
-    let { rects, rectsLength } = getRects()
+    let { rects } = getRects()
     const { buttonWidth, textContainerWidth } = getButtonWidth()
     moveOffsetLeft()
     if (rects[rects.length - 1].width + buttonWidth > textContainerWidth) {
@@ -72,23 +80,35 @@ function tightText() {
     }
 
 }
+/**
+ * 截取文本
+ * @param clampTag 一个用来判断需要进行截取的标志，只有clampTag的值为'canClamp'的时候，才可以调用clampText()，这样做是因为在文本展开的情况下，调整浏览器的宽度会引起截取文本的操作，这样展开的文本又会变成截取的，同时按钮的文本却还是停留在Collapse的情况，为了避免这种情况，才加的这个tag
+ */
 function clampText(clampTag?: string) {
+    // 当文本是未展开且clampTag的值为canClamp时才可以进行截取文本的操作。如果你不知道为什么要这么做，可以试试将下面一行的if判断去掉，然后将文本展开，随即调整浏览器宽度试试，你应该就明白了
     if (!expanded.value || clampTag == 'canClamp') {
+        // 首先进行一次截取，首次截取时是slice(0,0)，因为offset的初始值为0
         textRef.value && (textRef.value.textContent = props.text.slice(0, offset.value) + '...')
+        // 进行一个无限循环来大致寻找出文本的最终位置的大致位置
         while (true) {
             let { rects, rectsLength } = getRects()
             const { buttonWidth, textContainerWidth } = getButtonWidth()
+            // 当给定的maxLines会比所得的矩形个数（即真实行数）大的话，就往右移动进行截取
             if (props.maxLines > rectsLength) {
                 moveOffsetRight();
             } else if (props.maxLines < rectsLength) {
-                moveOffsetLeft();
+                moveOffsetLeft(); // 否则向左移动进行截取
             } else {
+                // 若给定行数和真实行数相等时，这个时候就要比较最后一个矩形的width加上按钮的width会不会超过文本容器的width了
+                // 当下面的if成立时，需要往右移动进行截取，因为这个else是给定行数和真实行数相等的情况，若不这么做，可能第三行就截取了几个字符就不能继续进行截取了
                 if (rects[rects.length - 1].width + buttonWidth <= textContainerWidth) {
                     moveOffsetRight();
-                    continue;
+                    continue; // 往右截取之后，需要continue再执行一次循环，若还是这个if的情况，继续向右截取
                 } else {
+                    // 这种情况就可能是最后一个矩形的width加上按钮的width超过了文本容器的width了，这个时候就要进行收紧文本，保证按钮会紧贴着文本
                     tightText()
                 }
+                // 最后记得要退出无限循环
                 break;
             }
         }
@@ -99,13 +119,12 @@ function clampText(clampTag?: string) {
  * 初始处理一下文本
  */
 function init() {
-    // 当按钮是tight时，先将未经截取的文本内容赋值给textRef，这样才能得到真实的文本行数；若按钮是oneLine时，将显示maxLines那么多行的含省略号的样式加上
+    // 当按钮是tight时，先将未经截取的文本内容赋值给textRef，因为只有这样才能得到真实的文本行数；若按钮是oneLine时，将显示maxLines那么多行的含省略号的样式加上即可
     props.buttonType == 'tight' ? (textRef.value && (textRef.value.textContent = props.text)) : $('#textRefSpan').css(clampClass.value)
     // 获取当前文本有多少行
     const rects = textRef.value?.getClientRects()
-    console.log('在init中:', rects);
     if (rects) {
-        // 当给定的maxLines的行数要比真正文本的行数还要小时，此时需要进行文本的截取
+        // 当给定的maxLines的行数要比真正文本的行数还要小或者刚好相等时，此时需要进行文本的截取
         if (props.maxLines <= rects.length) {
             clampText('canClamp')
         } else {
@@ -134,6 +153,7 @@ function toggle() {
         expanded.value = !expanded.value
     } else {
         // 当按钮的类型是tight类型时
+        // 若当前是未展开的情况，那么点击toggle，就要显示出所有的文本
         if (!expanded.value) {
             // 判断，若当前（即点击toggle之前）是收起的状态，那么需要将文本展开，显示未截取的原文本
             // 此时要将监听去掉，不然在mounted中的监听会让文本又变成省略的状态
@@ -145,7 +165,7 @@ function toggle() {
         }
         // 切换一下展开收起的状态
         expanded.value = !expanded.value
-        // 将监听加上
+        // 然后别忘记将监听加上
         props.buttonType == 'tight' && addListener(textClampRef.value as HTMLElement, () => {
             clampText()
         })
@@ -155,13 +175,9 @@ onMounted(() => {
     init()
     // 当按钮的类型是tight时才启动这个监听器
     if (textClampRef.value && props.buttonType == 'tight') {
-        try {
-            addListener(textClampRef.value as HTMLElement, () => {
-                clampText()
-            })
-        } catch (error) {
-            console.log('11:');
-        }
+        addListener(textClampRef.value as HTMLElement, () => {
+            clampText()
+        })
     }
 })
 onUnmounted(() => {
@@ -175,11 +191,9 @@ onUnmounted(() => {
 
 <template>
     <div ref="textClampRef">
-        <!-- <div ref="textClampContainerRef"> -->
         <span ref="textRef" id="textRefSpan"></span>
         <slot ref="toggleButtonRef" name="textExpandButton" :toggle="toggle" :buttonType="buttonType"
             :isExpanded="expanded"></slot>
-        <!-- </div> -->
     </div>
 </template>
 <style scoped></style>
